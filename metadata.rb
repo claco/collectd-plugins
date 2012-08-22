21144fd9fb506c4dc0744228bfb1b34915f8febf
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-08-22 10:02:09 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "noah@coderanger.net"
 license          "Apache 2.0"
 description      "Configure collectd plugins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.4"
+version          "1.0.5"
 depends          "collectd"
 supports         "ubuntu"
